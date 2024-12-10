1de4d445dc5d0539d07d46e47ba246ce0352f165
JazzLilo <jazminlilostar@gmail.com>
2024-12-10 11:21:29 -0400

    base loguin funcional

--- app/src/main/java/com/eris/super_ecomerce/Controller/LoginController.java
+++ app/src/main/java/com/eris/super_ecomerce/Controller/LoginController.java
@@ -67,4 +67,49 @@ public class LoginController {
             Log.e("LOGIN", "Error al crear la solicitud de inicio de sesión: ", e);
         }
     }
+
+
+    public void login_delivery  (LoginRequest loginRequest, LoginActivity loginActivity) {
+        try {
+            ClienteService clienteService = Utils.getRetrofitInstance().create(ClienteService.class);
+            Log.d(TAG, "DATOS " + loginRequest.toString());
+            Log.d("LOGIN", "Cuerpo de la solicitud: " + loginRequest.getEmail() + "  " + loginRequest.getPassword());
+
+            Call<Cliente> call = clienteService.login(loginRequest.getEmail(), loginRequest.getPassword());
+
+            call.enqueue(new Callback<Cliente>() {
+                @Override
+                public void onResponse(Call<Cliente> call, Response<Cliente> response) {
+                    if (response.isSuccessful()) {
+                        Log.d("LOGIN", "Respuesta exitosa: " + response.body());
+                        Cliente cliente = response.body();
+                        Log.d("LOGIN", "Datos Obtenidos: " + cliente.getNombre());
+
+                        if (cliente != null) {
+                            userData.saveUserData(cliente);
+                            Intent loginIntent = new Intent(loginActivity, MainActivity.class);
+                            loginActivity.startActivity(loginIntent);
+                            Toast.makeText(loginActivity,"Ingreso Exitoso",Toast.LENGTH_LONG);
+                        }
+                    } else {
+                        try {
+                            String errorBody = response.errorBody() != null ? response.errorBody().string() : "Error sin cuerpo";
+                            Log.d("LOGIN", "Error en la respuesta: " + errorBody);
+                        } catch (IOException e) {
+                            Log.e("LOGIN", "Error al leer el cuerpo de error", e);
+                        }
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Cliente> call, Throwable t) {
+                    Log.e("LOGIN", "Error en la llamada: ", t);
+                }
+            });
+        } catch (Exception e) {
+            Log.e("LOGIN", "Error al crear la solicitud de inicio de sesión: ", e);
+        }
+    }
+
+
 }
